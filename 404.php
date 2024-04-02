279595f7c4df7403e1d1aa3f7f9776b57742206d
AndreiFridrikhsen1 <200558923@student.georgianc.on.ca>
2024-04-01 23:09:52 -0400

    added 404 page  and .htaccess

---
+++ 404.php
@@ -0,0 +1,10 @@
+<?php
+$title ="Error"; 
+include("shared/header.php");
+?>
+<h1>404</h1>
+    <section class="<?php echo $title?>">
+        <p>page not found</p>
+    </section>
+</body>
+</html>
